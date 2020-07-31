920ad73a0ff1d6b076a3540015cb0d8920efdd1d
harshlohana <harshlohana38@gmail.com>
2020-07-31 07:35:54 +0530

    Fetch Api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // index.js
+
+
+console.log("I am live");
+
+
+
+var totalcases =()=> {
+
+fetch('https://api.covid19api.com/summary').then((apidata)=>{
+  //console.log(apidata);
+  return apidata.json();
+}).then((actualdata)=>{
+  console.log(actualdata);
+  //console.log(actualdata.Global.TotalConfirmed);
+
+  var totalcasesCount=actualdata.Global.TotalConfirmed;
+  document.getElementById('totalcases').innerHTML=`${totalcasesCount}`;
+
+  var totalDeaths=actualdata.Global.TotalDeaths;
+  document.getElementById('Deaths').innerHTML=`${totalDeaths}`;
+
+  var totalRecovered=actualdata.Global.TotalRecovered;
+  document.getElementById('Recovers').innerHTML=`${totalRecovered}`;
+
+  var newConfirmed=actualdata.Global.NewConfirmed;
+  document.getElementById('activeCases').innerHTML=`${newConfirmed}`;
+
+  var totalRecovered=actualdata.Global.TotalRecovered;
+  document.getElementById('totalRecovered').innerHTML=`${totalRecovered}`;
+
+  var newDeaths=actualdata.Global.NewDeaths;
+  document.getElementById('newDeaths').innerHTML=`${newDeaths}`;
+
+  var newRecovered=actualdata.Global.NewRecovered;
+  document.getElementById('newRecovered').innerHTML=`${newRecovered}`;
+
+
+});
+}
+
+totalcases();
